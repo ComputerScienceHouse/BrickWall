fd7b8d394c671c2d7177872d9b1c9320d7162a03
Lonnie Gerol <Lonniegerol@gmail.com>
2019-10-17 21:24:25 -0400

    add boilerplate code to initalize app and create route for locations

--- Brickwall/__init__.py
+++ Brickwall/__init__.py
@@ -1,3 +1,11 @@
 from flask import Flask
+from flask_restplus import Api, Resource
 
 app = Flask(__name__)
+app = Api(app = app)
+
+name_space = app.namespace('main', description='Main APIs')
+
+@name_space.route('/api/v1/location<location_id>', methods = ['GET, POST, PUT, DELE'])
+def location():
+    return "Hello World"
