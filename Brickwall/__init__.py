475a457ed2700599023a091175934f18e18a24c8
Lonnie Gerol <Lonniegerol@gmail.com>
2019-10-20 19:09:40 -0400

    finished creating skeleton for API

--- Brickwall/__init__.py
+++ Brickwall/__init__.py
@@ -1,11 +1,48 @@
 from flask import Flask
 from flask_restplus import Api, Resource
 
-app = Flask(__name__)
-app = Api(app = app)
+flask_app = Flask(__name__)
+app = Api(app = flask_app)
 
 name_space = app.namespace('main', description='Main APIs')
 
-@name_space.route('/api/v1/location<location_id>', methods = ['GET, POST, PUT, DELE'])
-def location():
-    return "Hello World"
+@name_space.route('/api/v1/location/<location_id>')
+class Location(Resource):
+    def get(self, location_id):
+        print("location get")
+        return {"location": location_id}
+    def post(self, location_id):
+        print("Location put")
+    def put(self):
+        print("Location put")
+    def delete(self):
+        print("Location delete")
+
+     
+
+@name_space.route('/api/vi/person<username>')
+class Person(Resource):
+    def get(self, username):
+        return {"person": username}
+
+@name_space.route('/api/v1/company/<company_id>')   
+class Company(Resource):
+    def get(self, company_id):
+        return {"company_id": company_id}
+    def post(self):
+        pass
+    def put(self):
+        pass
+    def delete(self):
+        pass
+
+@name_space.route('/api/v1/company/<company_id>/<review_id>')
+class Review(Resource):
+    def get(self, company_id, review_id):
+        return  {"review id": review_id, "company_id": company_id}
+    def post(self):
+        pass
+    def put(self):
+        pass
+    def delete(self):
+        pass
