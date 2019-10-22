2d867f9614cacdd3efb63fc2ed55f6377233faea
Lonnie Gerol <Lonniegerol@gmail.com>
2019-10-21 21:29:36 -0400

    added comment field to columns

--- Brickwall/models.py
+++ Brickwall/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Float
 from . import db
 from enum import Enum
 
@@ -7,52 +7,50 @@ from enum import Enum
 class Review(db.Model):
     __tablename__ = "review"
     review_id = Column(Integer, primary_key=True, nullable=False)
-    # user name of reviewer
-    member = Column(String, nullable=False)
-    # whether the person worked full time or co-op
-    # takes a string, but precondition is it should take one of two strings ("full-time", "co-op")
-    type = Column(String, nullable=False)
-    company_id = Column(ForeignKey('company.company_id'), nullable=False)
+    member = Column(String, nullable=False, comment="user-name of reviewer")
+    type = Column(String, nullable=False, comment = 
+    """whether the person worked full time or co-op, takes a string, but precondition is it should take 
+    one of two strings (\"full-time\" or \"co-op\")""")
+    company_id = Column(ForeignKey('company.company_id'), nullable=False, comment = \
+    "A reference to the company that the review is about")
 
 class Location:
     __tablename__ = "location"
     location_id = Column(Integer, primary_key=True, nullable=False)
-    # name of the location
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, comment = "The name of the location")
     # map_location - TBD
 
 class Interview:
     __tablename__ = "interview"
     interview_id = Column(Integer, primary_key=True, nullable=False)
     review_id = Column(ForeignKey('review.review_id'), nullable=False) 
-    # number of times the person interviewed
-    interview_count = Column(Integer, default=0, nullable=False)
-    # Whether there were any coding challenges during the interview
-    coding_challenges = Column(Boolean, nullable=False)
-    # whether the interview was on site or remote
-    # takes a string, but precondition is it should take one of two strings ("remote", "on-site")
-    interview_location = Column(String, nullable=False)
+    interview_count = Column(Integer, default=0, nullable=False, comment = \
+    "The number of times the person interviewed")
+    coding_challenges = Column(Boolean, nullable=False, comment=\
+     "Whether there were any coding challenges during the interview")
+    interview_location = Column(String, nullable=False, comment=\
+    """whether the interview was on site or remote takes a string, but precondition is it should take one of 
+    two strings ("remote", "on-site")""")
     # user comments about the interview
     body = Column(String, nullable=True)
 
 class Company:
     __tablename__ = "company"
     company_id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
-    website = Column(String, nullable=True)
+    name = Column(String, nullable=False, comment="The name of the company")
+    website = Column(String, nullable=True, comment= "The companies website")
 
 class Offer:
     __tablename__ = "offer"
     offer_id = Column(Integer, primary_key=True, nullable=True)
-    # review_id
-    pay = Column(Integer, nullable= False)
-    # takes a string, but precondition is it should take one of two strings ("salary", "hourly")
-    pay_type = Column(String, nullable= False)
-    # location
+    pay = Column(Float, nullable= False, comment= "The amount of money the reviewer was offered")
+    pay_type = Column(String, nullable= False, comment=\
+    "takes a string, but precondition is it should take one of two strings (\"salary\", \"hourly\")")
+    location = Column(ForeignKey('location.location_id'), nullable=False)
     offer_date = Column(DateTime, nullable=True)
     offer_deadline = Column(DateTime, nullable=True)
-    # takes a string, but precondition is it should take one of two strings ("stipend", "corporate", "none")
-    housing = Column(String, nullable=False)
+    housing = Column(String, nullable=False, comment=\
+    "takes a string, but precondition is it should take one of two strings (\"stipend\", \"corporate\", \"none\")")
     stipend = Column(Integer, nullable=False)
     # Not sure what this is for
     body = Column(String, nullable= True)
