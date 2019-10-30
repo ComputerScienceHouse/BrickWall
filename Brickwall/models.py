1f696ca94a640a7847c8c5f2770cb11659f40d32
Lonnie Gerol <Lonniegerol@gmail.com>
2019-10-29 21:27:25 -0400

    made all classes into database models

--- Brickwall/models.py
+++ Brickwall/models.py
@@ -6,25 +6,27 @@ class Review(db.Model):
     __tablename__ = "review"
     review_id = Column(Integer, primary_key=True, nullable=False)
     member = Column(String(55), nullable=False, comment="user-name of reviewer")
-    type = Column(String(10), nullable=False, comment = 
+    type = Column(String(10), nullable=False, comment=\
     """whether the person worked full time or co-op, takes a string, but precondition is it should take 
     one of two strings (\"full-time\" or \"co-op\")""")
     company_id = Column(ForeignKey('company.company_id'), nullable=False, comment = \
     "A reference to the company that the review is about")
 
-class Location:
+
+class Location(db.model):
     __tablename__ = "location"
     location_id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(256), nullable=False, comment = "The name of the location")
+    name = Column(String(256), nullable=False, comment="The name of the location")
     # map_location - TBD
 
-class Interview:
+
+class Interview(db.model):
     __tablename__ = "interview"
     interview_id = Column(Integer, primary_key=True, nullable=False)
-    review_id = Column(ForeignKey('review.review_id'), nullable=False) 
-    interview_count = Column(Integer, default=0, nullable=False, comment = \
+    review_id = Column(ForeignKey('review.review_id'), nullable=False)
+    interview_count = Column(Integer, default=0, nullable=False, comment=
     "The number of times the person interviewed")
-    coding_challenges = Column(Boolean, nullable=False, comment=\
+    coding_challenges = Column(Boolean, nullable=False, comment=
      "Whether there were any coding challenges during the interview")
     interview_location = Column(String(55), nullable=False, comment=\
     """whether the interview was on site or remote takes a string, but precondition is it should take one of 
@@ -32,17 +34,19 @@ class Interview:
     # user comments about the interview
     body = Column(String, nullable=True)
 
-class Company:
+
+class Company(db.model):
     __tablename__ = "company"
     company_id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(55), nullable=False, comment="The name of the company")
-    website = Column(String(256), nullable=True, comment= "The companies website")
+    website = Column(String(256), nullable=True, comment="The companies website")
 
-class Offer:
+
+class Offer(db.model):
     __tablename__ = "offer"
     offer_id = Column(Integer, primary_key=True, nullable=True)
-    pay = Column(Float, nullable= False, comment= "The amount of money the reviewer was offered")
-    pay_type = Column(String(10), nullable= False, comment=\
+    pay = Column(Float, nullable=False, comment="The amount of money the reviewer was offered")
+    pay_type = Column(String(10), nullable=False, comment=
     "takes a string, but precondition is it should take one of two strings (\"salary\", \"hourly\")")
     location = Column(ForeignKey('location.location_id'), nullable=False)
     offer_date = Column(DateTime, nullable=True)
@@ -51,13 +55,13 @@ class Offer:
     "takes a string, but precondition is it should take one of two strings (\"stipend\", \"corporate\", \"none\")")
     stipend = Column(Integer, nullable=False)
     # Not sure what this is for
-    body = Column(String, nullable= True)
+    body = Column(String, nullable=True)
 
-    
-class Job:
+
+class Job(db.model):
     __tablename__ = "job"
     job_id = Column(Integer, primary_key=True, nullable=False)
     review_id = Column(ForeignKey('review.review_id'), nullable=False)
     start_date = Column(DateTime, nullable=False)
-    end_date =  Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
     body = Column(String, nullable=True)
